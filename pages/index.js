76d2e9dff4f8da0e822b168b7ebc99b192b5f050
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-23 07:55:51 +0000

    ch1fin

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+const Home = () => {
+  return (
+    <>
+      <header>HEADER</header>
+
+      <main>
+        <div>
+          <h1>CUBE</h1>
+          <p>アウトプットしていくサイト</p>
+        </div>
+      </main>
+
+      <footer>FOOTER</footer>
+    </>
+  )
+}
+export default Home
