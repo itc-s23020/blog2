30876a30c0c5adf45395b0b27843c5de9ccc820e
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-24 06:40:51 +0000

    ch2.8fin

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,5 @@
 import Hero from 'components/hero'
-
-const About = () => {
-  return <Hero />
+const Home = () => {
+  return <Hero title='About' subtitle='About development activities' />
 }
-export default About
+export default Home
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Hero from 'components/hero'
 const Home = () => {
-  return <Hero />
+  return <Hero title='CUBE' subtitle='アウトプットしていくサイト' />
 }
 export default Home
