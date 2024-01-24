636622804ce64d0f72cd0736aad3fc37fde5c1b2
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-24 06:14:21 +0000

    ch2.6fin

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-import Header from 'components/header'
 import Hero from 'components/hero'
-import Footer from 'components/footer'
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-
-      <Footer />
-    </>
-  )
+  return <Hero />
 }
 export default Home
