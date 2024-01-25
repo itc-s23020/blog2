ca7cd5bb98d6252c2f40fc48932cd75243da5209
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-25 08:32:58 +0000

    ch4fin

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import 'styles/globals.css'
 import Layout from 'components/layout'
 
-function MyApp ({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return (
     <Layout>
       <Component {...pageProps} />
