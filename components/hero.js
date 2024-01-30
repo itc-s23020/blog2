513af7c6ba6355d95f51a2d9122b4bd36e172838
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-30 06:05:04 +0000

    ch5.6fin

--- components/hero.js
+++ components/hero.js
@@ -10,7 +10,14 @@ const Hero = ({ title, subtitle, imageOn = false }) => {
       </div>
       {imageOn && (
         <figure className={styles.image}>
-          <Image src={cube} alt='' layout='responsive' />{' '}
+          <Image
+            src={cube}
+            alt=''
+            layout='responsive'
+            sizes='(min-width: 1152px) 576px, (min-width: 768px) 50vw, 100vm'
+            priority
+            placeholder='blur'
+          />
         </figure>
       )}
     </div>
