a49085692758eb1e8a914ea0cceb26e060c8dbbc
itc-s23020 <s23020@std.it-college.ac.jp>
2024-01-31 05:55:55 +0000

    ch6.5

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-
+import { useRouter } from 'next/router'
+import siteImg from 'images/ogp.jpg'
 import { siteMeta } from 'lib/constants'
 const {
   siteTitle,
@@ -9,10 +10,19 @@ const {
   siteType,
   siteIcon
 } = siteMeta
-const Meta = ({ pageTitle, pageDesc }) => {
+
+const Meta = ({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) => {
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
 
   const desc = pageDesc ?? siteDesc
+
+  const router = useRouter()
+  const url = `${siteUrl}${router.asPath}`
+
+  const img = pageImg || siteImg.src
+  const imgW = pageImgW || siteImg.width
+  const imgH = pageImgH || siteImg.height
+  const imgUrl = img.startsWith('https') ? img : `${siteUrl}${img}`
   return (
     <Head>
       <title>{title}</title>
@@ -20,6 +30,21 @@ const Meta = ({ pageTitle, pageDesc }) => {
 
       <meta name='description' content={desc} />
       <meta property='og:description' content={desc} />
+
+      <link rel='canonical' href={url} />
+      <meta pproperty='og:url' content={url} />
+
+      <meta propety='og:site_name' content={siteTitle} />
+      <meta propety='og:type' content={siteType} />
+      <meta property='og:locale' content={siteLocale} />
+
+      <link rel='icon' href={siteIcon} />
+      <link rel='apple-touch-icon' href={siteIcon} />
+
+      <meta property='og:image' content={imgUrl} />
+      <meta property='og:image' content={imgW} />
+      <meta property='og:image' content={imgH} />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   )
 }
