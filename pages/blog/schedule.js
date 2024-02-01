63b967884e4194cdab2c1f646468bf37b5969f69
itc-s23020 <s23020@std.it-college.ac.jp>
2024-02-01 07:07:28 +0000

    ch8fin

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,4 +1,6 @@
 import { getPostBySlug } from 'lib/api'
+import { extractText } from 'lib/extract-text'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import PostHeader from 'components/post-header'
 import PostCategories from 'components/post-categories'
@@ -10,9 +12,24 @@ import {
 } from 'components/two-column'
 import ConvertBody from 'components/convert-body'
 import Image from 'next/image'
-const Schedule = ({ title, publish, content, eyecatch, categories }) => {
+const Schedule = ({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+  description
+}) => {
   return (
     <Container>
+      <Meta
+        pageTitle={title}
+        pageDesc={description}
+        pagrImg={eyecatch.url}
+        pageImgW={eyecatch.width}
+        pageImgH={eyecatch.height}
+      />
+
       <article>
         <PostHeader title={title} subtitle='Blog Article' publish={publish} />
         <figure>
@@ -46,13 +63,16 @@ const getStaticProps = async () => {
   const slug = 'schedule'
 
   const post = await getPostBySlug(slug)
+
+  const description = extractText(post.content)
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
       eyecatch: post.eyecatch,
-      categories: post.categories
+      categories: post.categories,
+      description
     }
   }
 }
