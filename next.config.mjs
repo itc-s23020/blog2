a63375948da0fafd667c470d2ea14741ca7685b0
itc-s23020 <s23020@std.it-college.ac.jp>
2024-02-01 02:28:22 +0000

    8.3fin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStricMode: true,
+  images: {
+    domains: ['images.microcms-assets.io']
+  }
+}
 
-export default nextConfig;
+export default nextConfig
